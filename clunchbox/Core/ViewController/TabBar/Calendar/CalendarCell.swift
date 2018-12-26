83bad19eb6484287e93a3839e8fd00bf9e28a60f
Eléa Reggiani <elea.reggiani@hetic.net>
2018-12-26 22:28:34 +0100

    Ecran Recette, Calendrier, Accueil
    Début recette (liste) + Continuité du calendrier (avec liste en dessous) + Collection view x2 sur Accueil pour voir les plats à venir et des idées recettes (en mode slide)

---
+++ clunchbox/Core/ViewController/TabBar/Calendar/CalendarCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  clunchbox
+//
+//  Created by Macintosh on 15/12/2018.
+//  Copyright © 2018 Eléa Reggiani. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarCell: JTAppleCell {
+    
+    @IBOutlet weak var cellDay: UILabel!
+}
--- clunchbox/Core/ViewController/TabBar/Calendar/CalendarViewController.swift
+++ clunchbox/Core/ViewController/TabBar/Calendar/CalendarViewController.swift
@@ -7,24 +7,73 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class CalendarViewController: UIViewController {
-
+   
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: JTAppleCalendarView!
+    
+    let formatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.collectionView.calendarDataSource = self
+        self.collectionView.calendarDelegate = self
         // Do any additional setup after loading the view.
+    }
+}
+
+extension CalendarViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 3
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let menuDayCell = self.tableView.dequeueReusableCell(withIdentifier: "menuDayCell", for: indexPath)
+        menuDayCell.textLabel?.text = "\(indexPath.row)"
+        
+        return menuDayCell
+    }
+    
+    
+}
+
+extension CalendarViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+      
+        return
+    }
+    
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2017 01 01")!
+         let endDate = formatter.date(from: "2017 12 31")!
+        
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return parameters
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let myCustomCell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "CalendarCell", for: indexPath) as! CalendarCell
+        
+        self.calendar(calendar, willDisplay: myCustomCell, forItemAt: date, cellState: cellState, indexPath: indexPath)
+        myCustomCell.cellDay.text = cellState.text
+        return myCustomCell
     }
-    */
 
 }
+
+
