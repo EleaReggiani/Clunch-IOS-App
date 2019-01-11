fb72d82f5c762ed6c1e3ed222eb72ebaa8c7c390
Eléa Reggiani <elea.reggiani@hetic.net>
2019-01-11 13:16:53 +0100

    Ajout des events natifs
    Ajout d'un view controller de test pour créer un évenement, l'éditer et voir le détail.
    Ajout dans info plist la demande d'autorisation aux données du calendrier natif.

---
+++ clunchbox/Core/GenericClasses/ClunchEvent.swift
@@ -0,0 +1,10 @@
+//
+//  ClunchEvent.swift
+//  clunchbox
+//
+//  Created by Macintosh on 06/01/2019.
+//  Copyright © 2019 Eléa Reggiani. All rights reserved.
+//
+
+import Foundation
+import EventKit
